7cf03a14c42ac341cbbf90f5dd015e53919715f3
Minsoo Choi <lucas0209@yonsei.ac.kr>
2023-04-04 15:11:38 +0900

    feat: 호감 목록 삭제 기능 추가
    - 사용자 소유권 및 권한 체크 예외처리 로직 추가.
    - 삭제 후 다시 호감목록 페이지로 Redirect.

--- src/main/java/com/bell/ringMyBell/boundedContext/likeInfo/controller/LikeInfoController.java
+++ src/main/java/com/bell/ringMyBell/boundedContext/likeInfo/controller/LikeInfoController.java
@@ -3,26 +3,34 @@ package com.bell.ringMyBell.boundedContext.likeInfo.controller;
 import com.bell.ringMyBell.base.request.RequestData;
 import com.bell.ringMyBell.base.response.ResponseData;
 import com.bell.ringMyBell.boundedContext.instaMember.entity.InstaMember;
+import com.bell.ringMyBell.boundedContext.instaMember.service.InstaMemberService;
 import com.bell.ringMyBell.boundedContext.likeInfo.entity.LikeInfo;
 import com.bell.ringMyBell.boundedContext.likeInfo.service.LikeInfoService;
+import com.bell.ringMyBell.boundedContext.member.entity.Member;
+import com.bell.ringMyBell.boundedContext.member.service.MemberService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/likeInfo")
 @RequiredArgsConstructor
+@Slf4j
 public class LikeInfoController {
     private final RequestData requestData;
     private final LikeInfoService likeInfoService;
+    private final InstaMemberService instaMemberService;
+    private final MemberService memberService;
 
     @GetMapping("/add")
     public String showAdd() {
@@ -55,4 +63,23 @@ public class LikeInfoController {
         }
         return "usr/likeInfo/list";
     }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Long id, Principal principal) {
+        Optional<LikeInfo> likeInfo = likeInfoService.findById(id);
+        Optional<Member> loginUser = memberService.findByUsername(principal.getName());
+
+        if (likeInfo.isPresent() && loginUser.isPresent()) {
+            if (likeInfoService.matches(likeInfo.get(), loginUser.get())) {
+                likeInfoService.delete(likeInfo.get());
+                return requestData.redirectWithMsg("/likeInfo/list", "정상적으로 삭제되었습니다.");
+            }
+            return requestData.redirectWithMsg("/likeInfo/list", "삭제 권한이 존재하지 않습니다.");
+        }
+        return requestData.redirectWithMsg("/likeInfo/list", "존재하지 않는 정보입니다.");
+    }
+
+
+
 }
