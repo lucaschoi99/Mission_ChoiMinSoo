d2d24ba302bf094029ed00409bc876f3ef2bc487
Minsoo Choi <lucas0209@yonsei.ac.kr>
2023-05-10 10:38:22 +0900

    feat: 백엔드 쿨타임 로직 위한 부가 기능

--- src/main/java/com/bell/ringMyBell/boundedContext/likeInfo/entity/LikeInfo.java
+++ src/main/java/com/bell/ringMyBell/boundedContext/likeInfo/entity/LikeInfo.java
@@ -1,6 +1,8 @@
 package com.bell.ringMyBell.boundedContext.likeInfo.entity;
 
+import com.bell.ringMyBell.base.response.ResponseData;
 import com.bell.ringMyBell.boundedContext.instaMember.entity.InstaMember;
+import com.bell.ringMyBell.util.Ut;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
@@ -41,6 +43,8 @@ public class LikeInfo {
 
     private int attractiveTypeCode; // 매력포인트 (1=외모, 2=성격, 3=능력)
 
+    private LocalDateTime modifyUnlockDate;
+
     public String getAttractiveTypeDisplayName() {
         return switch (attractiveTypeCode) {
             case 1 -> "외모";
@@ -52,4 +56,32 @@ public class LikeInfo {
     public void changeTypeCode(int code) {
         this.attractiveTypeCode = code;
     }
+
+    public boolean isModifyUnlocked() {
+        return modifyUnlockDate.isBefore(LocalDateTime.now());
+    }
+
+    // 초 단위에서 올림 해주세요.
+    public String getModifyUnlockDateRemainStrHuman() {
+        return Ut.time.diffFormat1Human(LocalDateTime.now(), modifyUnlockDate);
+    }
+
+    public ResponseData<LikeInfo> updateAttractionTypeCode(int attractiveTypeCode) {
+        if (this.attractiveTypeCode == attractiveTypeCode) {
+            return ResponseData.of("F-1", "이미 설정되었습니다.");
+        }
+
+        this.attractiveTypeCode = attractiveTypeCode;
+
+        return ResponseData.of("S-1", "성공");
+    }
+
+    public String getAttractiveTypeDisplayNameWithIcon() {
+        return switch (attractiveTypeCode) {
+            case 1 -> "<i class=\"fa-solid fa-person-rays\"></i>";
+            case 2 -> "<i class=\"fa-regular fa-face-smile\"></i>";
+            default -> "<i class=\"fa-solid fa-people-roof\"></i>";
+        } + "&nbsp;" + getAttractiveTypeDisplayName();
+    }
+
 }
